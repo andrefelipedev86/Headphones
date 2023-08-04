e5762ebcf1a233b8768fbfb9135fcac9f662b87b
André Felipe <66284955+andrefelipedev86@users.noreply.github.com>
2023-08-04 01:01:40 -0300

    Add files via upload
    Landing Page first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+
+function switchImg1() {
+   document.getElementById('mainImg').src = "./assets/product1.png"
+  
+}
+
+function switchImg2() {
+  document.getElementById('mainImg').src = "./assets/product2.png"
+ 
+}
+
+function switchImg3() {
+  document.getElementById('mainImg').src = "./assets/product3.png"
+ 
+}
